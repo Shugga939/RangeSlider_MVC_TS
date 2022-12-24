6e1f86fa517b513ee54efd034e6c81b750977486
Shugga939 <Shugga939@gmail.com>
2022-12-24 21:51:52 +0300

    added rangeLine tests

---
+++ src/MVC/View/subview/RangeLine/RangeLine.test.ts
@@ -0,0 +1,88 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { rotation } from '../../../../Types/Constants';
+import { Options } from '../../../../Types/Interfaces'
+import Observer from '../../../../Utils/Observer';
+import Handle from '../Handle/Handle';
+import RangeLine from './RangeLine';
+
+
+const options: Options = {
+  min_value: 0,
+  max_value: 100,
+  values: [20, 70],
+  separator: ' - ',
+  modifier: '',
+  range: true,
+  orientation: rotation.VERTICAL,
+  label: false,
+  step: 0,
+  marks: [
+    {
+      value: 0,
+      label: `${0}`
+    },
+    {
+      value: 100,
+      label: `${100}`
+    }]
+};
+
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
+
+describe('RangeLine test', () => {
+  const appElement = document.createElement('div')
+  const sliderElement = document.createElement('div')
+
+  const observer = new Observer()
+  const handle = new Handle(options, observer)
+  const rangeLine = new RangeLine(options, handle)
+
+  handle.render(appElement, 300)
+  const handle_1 = handle.getHandle1()
+  const handle_2 = handle.getHandle2()
+  
+  test('render method', () => {
+    const render = jest.spyOn(rangeLine, 'render');
+    const rangeLineElement = rangeLine.rangeLineElement
+    rangeLine.render(sliderElement)
+
+    expect(render).toHaveBeenCalled();
+    expect(render).toHaveBeenCalledTimes(1);
+
+    expect([...sliderElement.children].includes(rangeLineElement)).toBe(true)
+    expect(rangeLineElement.style.display).toBeDefined();
+    expect([...rangeLineElement.classList].includes('slider-range')).toBe(true)
+  });
+
+
+  test('init method', () => {
+    const updateStyle = jest.spyOn(RangeLine.prototype as any, '_updateStyle');
+    const update = jest.spyOn(rangeLine, 'update');
+    rangeLine.init(60, 210);
+
+    expect(updateStyle).toHaveBeenCalled();
+    expect(updateStyle).toHaveBeenCalledTimes(1);
+    expect(update).toHaveBeenCalled();
+    expect(update).toHaveBeenCalledTimes(1);
+  });
+
+
+  test('update and setOptions methods', () => {
+    rangeLine.setOptions(options, 100, 200)
+    const rangeLineElement = rangeLine.rangeLineElement
+    expect(rangeLineElement.style.height).toBe('100px')
+    expect(rangeLineElement.style.bottom).toBe('100px')
+
+    const changbleOptions : Options = {...options, range: false}
+    rangeLine.setOptions(changbleOptions, 100, 200)
+    expect(rangeLineElement.style.height).toBe('100px')
+    expect(rangeLineElement.style.bottom).toBe('0px')
+  });
+})
--- src/MVC/View/subview/RangeLine/RangeLine.ts
+++ src/MVC/View/subview/RangeLine/RangeLine.ts
@@ -3,16 +3,16 @@ import {rotation} from '../../../../Types/Constants'
 import Handle from '../Handle/Handle'
 
 export default class RangeLine {
-    rangeLine : HTMLElement
-    slider : HTMLElement
-    handle: Handle
-    options : Partial<Options>
-    half_width_handle : number
-    borderWidth_of_slider : number
-    isRange: boolean
-    isVertical: boolean
+    private rangeLine : HTMLElement
+    private slider : HTMLElement
+    private handle: Handle
+    // private options : Options
+    private half_width_handle : number
+    private borderWidth_of_slider : number
+    private isRange: boolean
+    private isVertical: boolean
   
-    constructor (options: Partial<Options>, handle: Handle) {
+    constructor (options: Options, handle: Handle) {
       this.handle = handle
       this.rangeLine  = document.createElement('span')
       this.isRange = (options.range == true)
@@ -26,14 +26,19 @@ export default class RangeLine {
     }
     
     init(first_value: number, second_value: number) {
-      this.updateStyle()
+      this._updateStyle()
       this.update(first_value, second_value)
     }
 
+      // for test
+    get rangeLineElement () {
+      return this.rangeLine
+    }
+
     setOptions (options: Options, first_value: number, second_value: number) : void {
-      this.options = options
+      // this.options = options
       this.isRange = options.range === true
-      this.updateStyle()
+      this._updateStyle()
       this.update(first_value, second_value)
     }
   
@@ -46,23 +51,26 @@ export default class RangeLine {
           that.rangeLine.style.height = `${second_value - first_value}px`
           that.rangeLine.style.bottom = `${first_value - that.borderWidth_of_slider}px`
         } else {
+          const sliderPaddingLeft = parseInt(getComputedStyle(that.slider).paddingLeft)
           that.rangeLine.style.width = `${second_value - first_value}px`
-          that.rangeLine.style.left = `${first_value + that.half_width_handle-parseInt(getComputedStyle(that.slider).paddingLeft)}px`
+          that.rangeLine.style.left = `${first_value + that.half_width_handle-sliderPaddingLeft}px`
         }
       }
 
       function update_if_notRange () {
         if (that.isVertical) {
-          that.rangeLine.style.height = `${first_value + parseInt(getComputedStyle(that.slider).paddingTop)}px`
+          const sliderPaddingTop = parseInt(getComputedStyle(that.slider).paddingTop)
+          that.rangeLine.style.height = `${first_value + sliderPaddingTop}px`
           that.rangeLine.style.bottom = `${-that.borderWidth_of_slider}px`
         } else {
-          that.rangeLine.style.width = `${first_value + parseInt(getComputedStyle(that.slider).paddingLeft)}px`
-          that.rangeLine.style.left = `${that.half_width_handle-parseInt(getComputedStyle(that.slider).paddingLeft)}px`
+          const sliderPaddingLeft = parseInt(getComputedStyle(that.slider).paddingLeft)
+          that.rangeLine.style.width = `${first_value + sliderPaddingLeft}px`
+          that.rangeLine.style.left = `${that.half_width_handle - sliderPaddingLeft}px`
         }
       }
     }
   
-    updateStyle () {
+    private _updateStyle () {
       this.half_width_handle = this.handle.getHandle1().offsetWidth/2   
       this.borderWidth_of_slider = this.isVertical? this.slider.clientTop : this.slider.clientLeft 
       const margin = this.half_width_handle + this.borderWidth_of_slider
